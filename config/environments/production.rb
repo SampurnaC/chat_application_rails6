4922c945ace9aaeb940eedb17a849e564762ce20
SampurnaC <sampurna.chapagainnn@gmail.com>
2020-11-11 17:14:30 +0545

    remove cached files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://frozen-taiga-59003.herokuapp.com/'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ 'http://frozen-taiga-59003.herokuapp.com/', 'https://frozen-taiga-59003.herokuapp.com/' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
